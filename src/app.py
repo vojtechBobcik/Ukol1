4ec76d0b54ce36512ca853fdac510fd954f8a40c
VojtechBobcik <xbobcik@mendelu.cz>
2022-10-01 22:34:58 +0200

    prvni ukol dodelany. oprava chyb pri cteni vstupu kdy python cetl i konec radku. dodelani pruchodu vstupem, pocitani sousedu a vypis max.

--- src/app.py
+++ src/app.py
@@ -4,68 +4,75 @@ import re
 from tokenize import String
 from typing import List
 
-
-class Graph:
-    graphNodes: List
-    graphEdges: List
-
-    def __init__(self) -> None:
-        self.graphNodes = []
-        self.graphEdges = []
-
-    def zjistiPocetZnamychProVsechnyUzly(self):
-        for x in self.graphNodes:
-            self.zjistiPocetZnamychProUzelZeSeznamuHran(x.nodeName,self.graphEdges)
-
-    def zjistiPocetZnamychProUzelZeSeznamuHran(self, name, edgeList):
-        print("hledam pocet znamych")
-
-        numberOfNeighbours = 0
-        for y in edgeList:
-            if (y.edgeFrom == name or y.edgeTo == name):
-                print(y.edgeFrom+" - "+ y.edgeTo)
-                numberOfNeighbours+=1
-                print(numberOfNeighbours)
-        print(x.nodeName+"("+repr(numberOfNeighbours)+")")
-
-
-
 class Node:
     def __init__(self,name) -> None:
         self.nodeName = name
         self.inputEdges = []
         self.outputEdges = []
         self.nodeValue = None
+    
+    def assignInputEdgeToNode(self, edge):
+        self.inputEdges.append(edge)
+
+    def assignOutputEdgeToNode(self, edge):
+        self.outputEdges.append(edge)
 
 
 class Edge:
-    edgeFrom : str
-    edgeTo: str
-    value: int
+    edgeFrom=None
+    edgeTo=None
+    value=None
 
     def __init__(self, edgeFrom, edgeTo) -> None:
         self.edgeFrom = edgeFrom
-        self.edgeTo = edgeTo
-  
+        self.edgeTo =edgeTo
+
+class Graph:
+    def __init__(self) -> None:
+        self.graphNodes = []
+        self.graphEdges = []
+    
+
+    def zjistiPocetZnamychProVsechnyUzly(self):
+        pocetNavazujicichUzlu={}
+        max = 0
+        for x in self.graphNodes:
+            name=x.nodeName
+            numberOfNeighbours = 0
+            for y in self.graphEdges:
+                #pri pruchodu mi na konci radku pridavalo \r
+                if (y.edgeFrom == x.nodeName or y.edgeTo[:-1] == x.nodeName):
+                    numberOfNeighbours= numberOfNeighbours +1
+            
+            pocetNavazujicichUzlu[name]=numberOfNeighbours
+            if(numberOfNeighbours>max): max=numberOfNeighbours
+
+        for i in pocetNavazujicichUzlu:
+            if pocetNavazujicichUzlu[i]==max:
+                print(i+"("+repr(pocetNavazujicichUzlu.get(i))+")")
 
 g = Graph()
 nodes = []
 
-# Cteni vstupniho seznamu uzlu ukol 1
+#precteni radku uzlu
+gr = input()
+groups = re.match(r"^Group:\s(.*)$", gr)
+if groups:
+    nodes = groups.group(1).split(", ")
+    nodes[-1] = nodes[-1][:-1]
+
+
+#pridani vsech uzlu ktere jsme nasli v cyklu do objektu graf. Pridavame vsechno najednou na konci cteni.
+for x in nodes:
+
+    g.graphNodes.append(Node(x))
+
+# Cteni vstupniho seznamu hran
 for line in sys.stdin:
-    #precteni radku uzlu
-    groups = re.match(r"^Group:\s(.*)$", line)
-    if groups:
-        nodes = groups.group(1).split(", ")
-        
     #precteni radku hrany
     edgeGroup = re.match(r"^(.*)\s\-\s(.*)", line)
     if edgeGroup:
         g.graphEdges.append(Edge(edgeGroup.group(1), edgeGroup.group(2)))
-
-#pridani vsech uzlu ktere jsme nasli v cyklu do objektu graf. Pridavame vsechno najednou na konci cteni.
-for x in nodes:
-    g.graphNodes.append(Node(x))
 
 
 g.zjistiPocetZnamychProVsechnyUzly()
