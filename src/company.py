87cb482e6329f85ebce743bcaae8315631954671
VojtechBobcik <xbobcik@mendelu.cz>
2022-10-03 19:25:05 +0200

    5 ukol rozpracovani

---
+++ src/company.py
@@ -0,0 +1,101 @@
+from operator import index
+from os import link
+from platform import node
+import sys
+import re
+from tokenize import String
+from typing import List
+
+class Graph:
+    def __init__(self) -> None:
+        self.graphNodes = []
+        self.graphEdges = []
+    
+    def findNodeInList(self, node, list):
+        for x in list:
+            if node.nodeName == x:
+                return True
+
+    sadySpolupracovniku = []
+    def kontrolaSpoluprace(self, projekty):
+        
+        for zkoumany in self.graphNodes:
+            spolupracovali = set()
+            print("zkoumany: " + zkoumany.nodeName)
+            
+            for pracovniciProjektu in projekty:
+                print("pracovnici na projektu: " + repr(pracovniciProjektu))
+                if self.findNodeInList(zkoumany,pracovniciProjektu):
+                    print("sem")
+                    for pracovnik in pracovniciProjektu:
+                        spolupracovali.add(pracovnik)
+                        print(" + ")
+            print("pocet spolupracovniku: " + repr(len(spolupracovali)))
+            if len(spolupracovali) != len(self.graphNodes):
+                print("False")
+                return False
+                       
+        print("True")
+        return True
+
+        
+
+class Node:
+    def __init__(self,name) -> None:
+        self.nodeName = name
+        self.inputEdges = []
+        self.outputEdges = []
+        self.nodeValue = None
+    
+    def assignInputEdgeToNode(self, edge):
+        self.inputEdges.append(edge)
+
+    def assignOutputEdgeToNode(self, edge):
+        self.outputEdges.append(edge)
+
+
+class Edge:
+    edgeFrom=None
+    edgeTo=None
+    value=None
+
+    def __init__(self, edgeFrom, edgeTo) -> None:
+        self.edgeFrom = edgeFrom
+        self.edgeTo =edgeTo
+
+g = Graph()
+nodes = []
+
+#precteni radku uzlu
+gr = input()
+groups = re.match(r"^Employ\:(.*)$", gr)
+if groups:
+    nodes = groups.group(1).split(", ")
+    nodes[-1] = nodes[-1][:-1]
+
+for x in nodes:
+    g.graphNodes.append(Node(x))
+    
+
+#pridani vsech uzlu ktere jsme nasli v cyklu do objektu graf. Pridavame vsechno najednou na konci cteni.
+#update: pridani do setu
+
+
+pracovniciNaProjektu = []
+# Cteni vstupniho seznamu hran
+for line in sys.stdin:
+    #precteni nazvu linky
+    projekt = re.match(r"^(.*)\:\s",line)
+    pracovnikNaProjektu = line.split(projekt.group(1))
+    #oseknutí mezery a dvojtečky zepředu a znaku konce radku zezadu
+    pracovnikNaProjektu = pracovnikNaProjektu[1][2:-2]
+    edgeParts = re.split(r"\,\s", pracovnikNaProjektu)
+    pracovniciNaProjektu.append(edgeParts)
+
+g.kontrolaSpoluprace(pracovniciNaProjektu)
+
+
+
+
+    
+    
